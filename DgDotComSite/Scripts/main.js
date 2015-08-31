a61c4afd58021708c2d618984a53bb46923bd9a2
Dane <dgrace@mykolab.com>
2015-08-31 16:03:14 -0700

    Initial Check in
    CHECK IN ALL THE THINGS!

---
+++ DgDotComSite/Scripts/main.js
@@ -0,0 +1,46 @@
+/// <reference path="_references.js" />
+$(document).ready(function() {
+	populateNav();
+	populateTitle();
+	populateFooter();
+});
+
+var populateNav = function() {
+	var navSpacer = "\xa0 \xa0 \xa0 \xa0";
+	var elem = $("#header");
+	
+	var navItem = "<a href='/dg/default.html' class='header-link'>MAIN</a>";
+	elem.append(navItem);
+	elem.append(navSpacer);
+	
+	navItem = "<a href='/dg/about.html' class='header-link'>ABOUT</a>";
+	elem.append(navItem);
+	elem.append(navSpacer);
+	
+	navItem = "<a href='/dg/links.html' class='header-link'>LINKS</a>";
+	elem.append(navItem);
+	elem.append(navSpacer);
+	
+	navItem = "<a href='/dg/contact.html' class='header-link'>CONTACT</a>";
+	elem.append(navItem);
+}
+
+var populateTitle = function() {
+	$("#title").append("TEST DOT COM");
+}
+
+var populateFooter = function() {
+	var footerSpacer = "\xa0 \xa0 \xa0 \xa0";
+	var elem = $("#footerText");
+	
+	var footerItem = "<a href='/contact.html' target='_blank' class='footer-link'>dg dot com</a>";
+	elem.append(footerItem);
+	elem.append(footerSpacer);
+	
+	footerItem = "<a href='http://creativecommons.org/licenses/by/4.0/' target='_blank' class='footer-link'>Released under CC BY 4.0</a>"  ;
+	elem.append(footerItem);
+	elem.append(footerSpacer);
+	
+	footerItem = "<a href='http://github.com/invisibledane/DG-dot-com' target='_blank' class='footer-link'>Github</a>";
+	elem.append(footerItem);
+}
